d4260a55dfcdca1a3845f2d7a118b32ef0c22400
berkaycimsir <berkaycimsir2003@hotmail.com>
2022-06-12 02:34:49 +0300

    define id for users

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,8 +2,11 @@ import { AppProps } from 'next/app';
 import { withTRPC } from '@/lib/withTRPC';
 import { AppType } from 'next/dist/shared/lib/utils';
 import 'styles/root.css';
+import { setUserId } from '@/utils/userId';
+import { isBrowser } from '@/lib/isBrowser';
 
 const MyApp = (({ Component, pageProps }: AppProps) => {
+  if (isBrowser) setUserId();
   return <Component {...pageProps} />;
 }) as AppType;
 
